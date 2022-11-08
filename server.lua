ee4bd11e3dd999c7b8ff786ff1e6013142534089
RowDog <mrdiamonddirt@gmail.com>
2022-11-08 18:05:55 +0000

    🖥 QB-core and standalone compatible

--- server.lua
+++ server.lua
@@ -1,28 +1,34 @@
-local QBCore = exports['qb-core']:GetCoreObject()
-
--- RegisterCommand("riot", function(source, args, raw)
---     local src = source
---     TriggerClientEvent("Riot:ToggleRiot", src)
--- end)
-
--- RegisterCommand("riot2", function(source, args, raw)
---     local src = source
---     TriggerClientEvent("Riot:ToggleBalRiot", src)
--- end)
-QBCore.Commands.Add("riotshield", "Gives Riot Shield (Police Only)", {}, false, function(source)
-    local Player = QBCore.Functions.GetPlayer(source)
-    if Player.PlayerData.job.name == "police" then
+if QBCore == nil then
+    RegisterCommand("riot", function(source, args, raw)
+        local src = source
         TriggerClientEvent("Riot:ToggleRiot", src)
-    else
-        TriggerClientEvent('QBCore:Notify', source,  "For Police Officer Only", "error")
-    end
-end)
-
-QBCore.Commands.Add("riot2", "Gives Ballistic Riot Shield (Police Only)", {}, false, function(source)
-    local Player = QBCore.Functions.GetPlayer(source)
-    if Player.PlayerData.job.name == "police" then
+    end)
+    
+    RegisterCommand("riot2", function(source, args, raw)
+        local src = source
         TriggerClientEvent("Riot:ToggleBalRiot", src)
+    end)
     else
-        TriggerClientEvent('QBCore:Notify', source,  "For Police Officer Only", "error")
+        local QBCore = exports['qb-core']:GetCoreObject()
+    
+        QBCore.Commands.Add("riotshield", "Gives Riot Shield (Police Only)", {}, false, function(source)
+            local Player = QBCore.Functions.GetPlayer(source)
+            if Player.PlayerData.job.name == "police" then
+                TriggerClientEvent("Riot:ToggleRiot", source)
+            else
+                TriggerClientEvent('QBCore:Notify', source,  "For Police Officer Only", "error")
+            end
+        end)
+        
+        QBCore.Commands.Add("riot2", "Gives Ballistic Riot Shield (Police Only)", {}, false, function(source)
+            local Player = QBCore.Functions.GetPlayer(source)
+            if Player.PlayerData.job.name == "police" then
+                TriggerClientEvent("Riot:ToggleBalRiot", source)
+            else
+                TriggerClientEvent('QBCore:Notify', source,  "For Police Officer Only", "error")
+            end
+        end)
     end
-end)
+    
+    
+    
