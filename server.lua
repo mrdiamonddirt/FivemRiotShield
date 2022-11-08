9ba9f411b5218ff1564fcc8403d58af584d58979
RowDog <mrdiamonddirt@gmail.com>
2022-11-08 17:40:34 +0000

    🛡 added QB-core commands

--- server.lua
+++ server.lua
@@ -1,9 +1,28 @@
-RegisterCommand("riot", function(source, args, raw)
-    local src = source
-    TriggerClientEvent("Riot:ToggleRiot", src)
+local QBCore = exports['qb-core']:GetCoreObject()
+
+-- RegisterCommand("riot", function(source, args, raw)
+--     local src = source
+--     TriggerClientEvent("Riot:ToggleRiot", src)
+-- end)
+
+-- RegisterCommand("riot2", function(source, args, raw)
+--     local src = source
+--     TriggerClientEvent("Riot:ToggleBalRiot", src)
+-- end)
+QBCore.Commands.Add("riotshield", "Gives Riot Shield (Police Only)", {}, false, function(source)
+    local Player = QBCore.Functions.GetPlayer(source)
+    if Player.PlayerData.job.name == "police" then
+        TriggerClientEvent("Riot:ToggleRiot", src)
+    else
+        TriggerClientEvent('QBCore:Notify', source,  "For Police Officer Only", "error")
+    end
 end)
 
-RegisterCommand("riot2", function(source, args, raw)
-    local src = source
-    TriggerClientEvent("Riot:ToggleBalRiot", src)
+QBCore.Commands.Add("riot2", "Gives Ballistic Riot Shield (Police Only)", {}, false, function(source)
+    local Player = QBCore.Functions.GetPlayer(source)
+    if Player.PlayerData.job.name == "police" then
+        TriggerClientEvent("Riot:ToggleBalRiot", src)
+    else
+        TriggerClientEvent('QBCore:Notify', source,  "For Police Officer Only", "error")
+    end
 end)
